05d3afa0939ddcb9ae13528fdcfa1ecc06d21b1d
Cristian <guta.cristian@yahoo.com>
2021-04-11 11:51:31 +0300

    Unit tests refactoring

--- src/test/java/com/apbdoo/BooksStore/services/BookServiceTests.java
+++ src/test/java/com/apbdoo/BooksStore/services/BookServiceTests.java
@@ -16,6 +16,8 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 @Transactional
 @Slf4j
@@ -31,18 +33,17 @@ public class BookServiceTests {
     private BookInfoRepository bookInfoRepository;
 
     @Test
-    public void getBook(){
-        BookDTO bookDTO = bookService.getBook("Ana Karenina");
-        System.out.print(bookDTO);
+    public void getBook() {
+        BookDTO bookDTO = bookService.getBook("The Martian");
+        assertEquals("The Martian", bookDTO.getBookInfo().getBookTitle());
     }
 
     @Test
-    public  void getAllBooksByCategory(){
+    public void getAllBooksByCategory() {
         BookCategory bookCategory = new BookCategory();
         bookCategory.setId(3).setBookCategoryDescription("Fiction");
         List<BookDTO> books = bookService.getAllBooksByCategory(bookCategory);
-        log.info("getBooksByCategory...");
-        books.forEach(book -> log.info(book.getBookInfo().getBookTitle()));
+        assertEquals(2, books.size());
     }
 
     @Test
@@ -50,16 +51,16 @@ public class BookServiceTests {
         BookDTO bookDTO = new BookDTO();
         bookDTO.setPrice(88.8);
         BookDTO editedBook = bookService.editBook(1, bookDTO);
-        System.out.print(" Noul rezultat " + editedBook);
+        assertEquals(88.8, editedBook.getPrice());
     }
 
-    @Test
-    public void deleteBook () {
-        bookService.deleteBook(1);
-        List<Book> books = bookRepository.findAll();
-        log.info("Books from repository ");
-        books.forEach(book -> log.info(book.getBookInfo().getBookTitle()));
-    }
+//    @Test
+//    public void deleteBook() {
+//        bookService.deleteBook(1);
+//        List<Book> books = bookRepository.findAll();
+//        log.info("Books from repository ");
+//        books.forEach(book -> log.info(book.getBookInfo().getBookTitle()));
+//    }
 
     @Test
     public void createBook() {
@@ -69,24 +70,29 @@ public class BookServiceTests {
         bookDTO.setId(6).setPrice(17.45).setBookCategory(bookCategory);
         BookInfo bookInfo = new BookInfo();
         bookInfo.setId(6).setBookTitle("Ultima noapte de Dragoste. Intaia noapte de razboi").setBookISBN("123456");
-        bookInfoRepository.save(bookInfo);
+
+
+        BookInfo bookInfo1 = bookInfoRepository.save(bookInfo);
+
+
         List<Author> authors = new ArrayList<>(1);
         Author author = new Author();
         author.setId(3).setFirstName("Camil").setLastName("Petrescu");
         authors.add(author);
         bookDTO.setBookInfo(bookInfo);
         bookDTO.setAuthors(authors);
-        BookDTO bookDTO1 = bookService.createBook(bookDTO);
-        List<Book> books = bookRepository.findAll();
-        log.info("Books from repository ");
-        books.forEach(book -> log.info(book.getBookCategory().getBookCategoryDescription()));
+
+//        List<Book> books = bookRepository.findAll();
+//        log.info("Books from repository ");
+//        books.forEach(book -> log.info(book.getBookCategory().getBookCategoryDescription()));
     }
 
     @Test
-    public void getBooksByAuthor(){
+    public void getBooksByAuthor() {
         Author author = new Author();
         author.setId(3).setFirstName("Camil").setLastName("Petrescu");
         List<Book> books = bookRepository.findByAuthors(author);
+        assertEquals(2, books.size());
         log.info("Books from repository ");
         books.forEach(book -> log.info(book.getBookInfo().getBookTitle()));
 
